8da8bfcf398805d6a69ef3d0178bd0e94e2f22f8
alexanndrra <sashavornezh@icloud.com>
2023-09-14 11:52:08 -0500

    added ids for tabs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,16 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.material.tabs.TabItem;
+
 public class MainActivity extends AppCompatActivity {
 
+    TabItem tabItemDog;
+    TabItem tabItemCat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void tabClicked(View v){
-
+       
     }
 
 }
