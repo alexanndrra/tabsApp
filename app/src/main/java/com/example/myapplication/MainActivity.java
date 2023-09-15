892a83383e899daf522da216e1476af994812d45
alexanndrra <70533521+alexanndrra@users.noreply.github.com>
2023-09-15 03:25:12 +0300

    finished code for tabs

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,26 +1,40 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    TabItem tabItemDog;
-    TabItem tabItemCat;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tabItemDog = findViewById(R.id.dogsTabItem);
-        tabItemCat = findViewById(R.id.catsTabItem);
 
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager()
+                , FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        vpAdapter.addFragment(new fragmentDog(), "DOG");
+        vpAdapter.addFragment(new fragmentCat(), "CAT");
+
+        viewPager.setAdapter(vpAdapter);
     }
 
-    public void tabClicked(View v){
-    }
+
 
 }
